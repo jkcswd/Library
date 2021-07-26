44494ce597d0db8e180984431483b139714f9132
James Spencer <jameskcs1254@gmail.com>
2021-07-26 16:07:00 +0100

    Insert book constructor function created earlier

---
+++ library.js
@@ -0,0 +1,14 @@
+// title author pages read/not
+
+//constructor 
+
+function Book(title,author,pages,read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}.`
+    }
+}
